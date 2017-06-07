d5bbe9d883f3d4cf9c36d94e51599d85c835ed58
Pontus Nagy <pontusnagy@gmail.com>
2017-06-07 14:25:25 +0200

    ubersicht: ignore the network.out file, create it instead

--- install/osx/ubersicht.sh
+++ install/osx/ubersicht.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-## Symlink Ubersicht
+## Symlink Ubersicht and create the network.out file to display network data
 
 # Symlink
 ln -fs $HOME/dotfiles/conf/osx/ubersicht/WidgetSettings.json $HOME/Library/Application\ Support/tracesOf.Uebersicht/
+
+# Create the network.out file
+touch $HOME/dotfiles/conf/osx/ubersicht/widgets/nerdbar.widget/scripts/network.out
