4ec44e26692bdb2094a67eb29026d9a8710d3c35
Pontus Nagy <pontusnagy@gmail.com>
2017-06-08 11:48:27 +0200

    nvim: update nvim symlink for .vimrc

--- install/osx/neovim.sh
+++ install/osx/neovim.sh
@@ -5,6 +5,6 @@ mkdir -p $HOME/.config/nvim/autoload
 wget -O $HOME/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Symlink
-ln -fs $HOME/dotfiles/conf/osx/neovim/init.vim $HOME/.config/nvim/
+ln -fs $HOME/dotfiles/conf/osx/neovim/.vimrc $HOME/.config/nvim/init.vim
 ln -fs $HOME/dotfiles/conf/osx/neovim/colors.vim $HOME/.config/nvim/autoload/
 ln -fs $HOME/dotfiles/conf/osx/neovim/UltiSnips $HOME/.config/nvim/
