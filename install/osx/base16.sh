08adc10da8766551d760b78d092d74f71a2f3d6c
Pontus Nagy <pontusnagy@gmail.com>
2017-06-13 11:46:28 +0200

    base16: ignore the settings file, just create it on installation

--- install/osx/base16.sh
+++ install/osx/base16.sh
@@ -6,5 +6,8 @@
 # see: https://github.com/chriskempson/base16-shell
 git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
 
+# base16 settings file
+touch $HOME/dotfiles/conf/osx/base16/.base16
+
 # symlink
 ln -fs $HOME/dotfiles/conf/osx/base16/base16-p1xelHer0.sh $HOME/.config/base16-shell/scripts
