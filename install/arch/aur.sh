a5e2137ff53fc6c781e2a16cb81068e8e7e3c78a
Pontus Nagy <pontusnagy@gmail.com>
2017-11-17 06:33:27 +0100

    arch: added some aur packages

--- install/arch/aur.sh
+++ install/arch/aur.sh
@@ -5,11 +5,14 @@
 # packages to install
 packages=(
   https://aur.archlinux.org/cava.git
+  https://aur.archlinux.org/cower.git
   https://aur.archlinux.org/discord.git
   https://aur.archlinux.org/fasd-git.git
+  https://aur.archlinux.org/firefox-nightly.git
   https://aur.archlinux.org/gohufont.git
   https://aur.archlinux.org/google-chrome.git
   https://aur.archlinux.org/i3-gaps-git.git
+  https://aur.archlinux.org/jfbview.git
   https://aur.archlinux.org/lemonbar-git.git
   https://aur.archlinux.org/ncurses5-compat-libs.git
   https://aur.archlinux.org/neofetch.git
@@ -18,6 +21,8 @@ packages=(
   https://aur.archlinux.org/ruby-neovim.git
   https://aur.archlinux.org/spotify.git
   https://aur.archlinux.org/stack-static.git
+  https://aur.archlinux.org/ttf-iosevka.git
   https://aur.archlinux.org/ttf-twemoji-color.git
+  https://aur.archlinux.org/universal-ctags-git.git
   https://aur.archlinux.org/xbanish.git
 )
