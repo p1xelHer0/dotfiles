f14074d768be9595e2ebab04129f6a454644158e
Pontus Nagy <pontusnagy@gmail.com>
2017-11-18 23:48:26 +0100

    vim: added install to symlink the `after` folder

--- install/osx/neovim.sh
+++ install/osx/neovim.sh
@@ -5,3 +5,4 @@
 # symlink
 ln -fs "$HOME/dotfiles/conf/neovim/.vimrc" "$HOME/.config/nvim/init.vim"
 ln -fs "$HOME/dotfiles/conf/neovim/UltiSnips" "$HOME/.config/nvim/"
+ln -fs "$HOME/dotfiles/conf/neovim/after" "$HOME/.config/nvim/"
