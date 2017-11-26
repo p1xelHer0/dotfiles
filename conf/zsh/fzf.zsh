03892098237e98f130adbdc80a86d30015cf048e
Pontus Nagy <pontusnagy@gmail.com>
2017-11-26 07:19:06 +0100

    fzf: added alt-c settings

--- conf/zsh/fzf.zsh
+++ conf/zsh/fzf.zsh
@@ -25,6 +25,9 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # see https://github.com/junegunn/fzf/wiki/Configuring-shell-key-bindings
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 
+# use the same command with <A-c>
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+
 # }}}
 
 
