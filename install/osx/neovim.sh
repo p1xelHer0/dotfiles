c73a02a90ea384c0f786ed8449c4a45e4f3f9062
Pontus Nagy <pontusnagy@gmail.com>
2018-08-31 21:32:34 +0200

    vim: symlink colors as well

--- install/osx/neovim.sh
+++ install/osx/neovim.sh
@@ -6,3 +6,4 @@
 ln -fs "$HOME/dotfiles/conf/neovim/.vimrc" "$HOME/.config/nvim/init.vim"
 ln -fs "$HOME/dotfiles/conf/neovim/UltiSnips" "$HOME/.config/nvim/"
 ln -fs "$HOME/dotfiles/conf/neovim/after" "$HOME/.config/nvim/"
+ln -fs "$HOME/dotfiles/conf/neovim/colors" "$HOME/.config/nvim/"
