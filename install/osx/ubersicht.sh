9142c7c503e960183da21d01ebdff9bbd369185e
Pontus Nagy <pontusnagy@gmail.com>
2019-02-10 22:27:31 +0100

    escape backslashes in path

--- install/osx/ubersicht.sh
+++ install/osx/ubersicht.sh
@@ -3,7 +3,7 @@
 # symlink Ubersicht and create the network.out file to display network data
 
 # symlink
-ln -fs "$HOME/dotfiles/conf/osx/ubersicht/WidgetSettings.json" "$HOME/Library/Application\ Support/tracesOf.Uebersicht/"
+ln -fs "$HOME/dotfiles/conf/osx/ubersicht/WidgetSettings.json" "$HOME/Library/Application\\ Support/tracesOf.Uebersicht/"
 
 # create the network.out file
 touch "$HOME/dotfiles/conf/osx/ubersicht/widgets/nerdbar.widget/scripts/network.out"
