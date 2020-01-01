6968a6c4295d24b92b0c79337bedf12b30761fc4
Pontus Nagy <pontusnagy@gmail.com>
2020-01-01 11:21:16 +0100

    remove rbenv jenv and unused shit thx

--- install/osx/ruby.sh
+++ install/osx/ruby.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-# install latest version of Ruby and gems with rbenv
-# install Neovim dependency
-
-if test "$(which rbenv)"
-then
-  echo "Installing a stable version of Ruby..."
-
-  # get latest Ruby version
-  rubyv=$(rbenv install -l | grep -v - | tail -1)
-
-  # install Ruby
-  rbenv install "$rubyv"
-
-  # switch to the installed version
-  rbenv global "$rubyv"
-
-  # ruby dependency for Neovim
-  gem install neovim
-
-  # bundler and Cocoapods for react-native
-  # gem install bundler cocoapods
- fi
+# neovim
+gem install neovim
