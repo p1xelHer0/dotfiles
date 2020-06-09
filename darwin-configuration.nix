b5ac18a57815ee8814fcaf16ade5f98638cf746b
Pontus Nagy <pontusnagy@gmail.com>
2020-06-09 22:52:40 +0200

    very wip, nix

---
+++ darwin-configuration.nix
@@ -0,0 +1,70 @@
+{ config, pkgs, ... }:
+
+let inherit (pkgs) lorri;
+
+in {
+  system.stateVersion = 4;
+
+  environment.systemPackages = [ lorri ];
+
+  services.skhd.enable = true;
+
+  services.yabai = {
+    enable = true;
+    package = pkgs.yabai;
+    enableScriptingAddition = false;
+    config = {
+      focus_follows_mouse = "autoraise";
+      mouse_follows_focus = "off";
+      window_placement = "second_child";
+      window_opacity = "off";
+      window_opacity_duration = "0.0";
+      window_border = "on";
+      window_border_placement = "inset";
+      window_border_width = 2;
+      window_border_radius = 3;
+      active_window_border_topmost = "off";
+      window_topmost = "on";
+      window_shadow = "float";
+      active_window_border_color = "0xff5c7e81";
+      normal_window_border_color = "0xff505050";
+      insert_window_border_color = "0xffd75f5f";
+      active_window_opacity = "1.0";
+      normal_window_opacity = "1.0";
+      split_ratio = "0.50";
+      auto_balance = "on";
+      mouse_modifier = "fn";
+      mouse_action1 = "move";
+      mouse_action2 = "resize";
+      layout = "bsp";
+      top_padding = 10;
+      bottom_padding = 10;
+      left_padding = 10;
+      right_padding = 10;
+      window_gap = 10;
+    };
+
+    extraConfig = ''
+      # rules
+      yabai -m rule --add app='System Preferences' manage=off
+    '';
+  };
+
+  # Copied verbatim from https://github.com/iknow/nix-channel/blob/7bf3584e0bef531836050b60a9bbd29024a1af81/darwin-modules/lorri.nix
+  launchd.user.agents = {
+    "lorri" = {
+      serviceConfig = {
+        WorkingDirectory = (builtins.getEnv "HOME");
+        EnvironmentVariables = { };
+        KeepAlive = true;
+        RunAtLoad = true;
+        StandardOutPath = "/var/tmp/lorri.log";
+        StandardErrorPath = "/var/tmp/lorri.log";
+      };
+      script = ''
+        source ${config.system.build.setEnvironment}
+        exec ${lorri}/bin/lorri daemon
+      '';
+    };
+  };
+}
