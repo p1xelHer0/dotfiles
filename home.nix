07cf00994c985cf2ba4cf7ee1314affdc647bbbb
Pontus Nagy <pontusnagy@gmail.com>
2020-06-29 22:59:15 +0200

    Removed unused stuff, upped home.nix

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@
     curl
     direnv
     fasd
-    fzf
+    fswatch
     gitAndTools.diff-so-fancy
     gnupg
     htop
@@ -19,10 +19,9 @@
     shellcheck
     tmux
     wget
-    zsh
 
     # keyboards
-    dfu-util
+    # dfu-util
 
     # window manager
     skhd
@@ -33,23 +32,22 @@
     rnix-lsp
 
     # haskell
-    cabal-install
-    stack
+    # cabal-install
+    # stack
 
     # ocaml
     bs-platform
     opam
 
     # rust
-    rustup
+    # rustup
   ];
 
   programs.zsh = {
     enable = true;
 
-    enableAutosuggestions = true;
+    enableAutosuggestions = false;
     enableCompletion = true;
-    defaultKeymap = "vicmd";
 
     sessionVariables = {
       EDITOR = "nvim";
@@ -64,309 +62,154 @@
       export PATH=$DOTS_BIN:$PATH
       export PATH=$DOTS_DARWIN_BIN:$PATH
       export PATH=$YARN_BIN:$PATH
+
+      # profile zsh
+      # zmodload zsh/zprof
     '';
 
     initExtra = ''
-      if [[ $(uname) == "Darwin" ]]; then
-        DARWIN=true
-      fi
+      source $HOME/dev/repo/private/puck/puck.zsh
 
-      # =============================================================================
-      # settings {{{
-      # =============================================================================
+      eval "$(fasd --init auto)"
 
-      # theme
-      ZSH_THEME=puck
-
-      # use the maximum amount of file descriptors
-      ulimit -n 24576
-
-      export LC_ALL=en_US.UTF-8
-
-
-      # }}}
-      # =============================================================================
-      # oh-my-zsh {{{
-      # =============================================================================
-
-      export ZSH=$HOME/.oh-my-zsh
-
-
-      # }}}
-      # =============================================================================
-      # $PATH {{{
-      # =============================================================================
-
-      # dotfile scripts
-      export DOT_SCRIPTS=$HOME/dotfiles/bin
-
-      # Yarn
-      export YARN_BIN=$HOME/.yarn/bin
-
-      # Rust
-      # export CARGO_HOME=$HOME/.cargo
-
-
-      # set $PATH
-      export PATH=$DOT_SCRIPTS:$PATH
-
-      export PATH=$YARN_BIN:$PATH
-
-      # export PATH=$STACK_PACKAGES:$PATH
-
-      # export PATH=$HASKELL_IDE_ENGINE:$PATH
-
-      # export PATH=$CARGO_HOME:$PATH
-
-      # -----------------------------------------------------------------------------
-      # macOS $PATH
-      # -----------------------------------------------------------------------------
-
-      if [[ -n "$DARWIN" ]]; then
-        # macOS dotfile scripts
-        export MACOS_DOT_SCRIPTS=$HOME/dotfiles/bin/_darwin
-
-        # clang
-        # export CLANG=/usr/local/opt/llvm/bin
-
-        # React Native
-        # export REACT_DEBUGGER="unset ELECTRON_RUN_AS_NODE && open -g 'rndebugger://set-debugger-loc?port=19001' ||" yarn start
-
-        # xcode simctl is needed for react-native emulator
-        # export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
-
-        # Mono
-        # export MONO_ROOT=/Library/Frameworks/Mono.framework/Versions/Current/bin
-
-        # Android
-        # export ANT_HOME=/usr/local/opt/ant
-        # export MAVEN_HOME=/usr/local/opt/maven
-        # export GRADLE_HOME=/usr/local/opt/gradle
-        # export ANDROID_HOME=/usr/local/opt/android-sdk
-        # export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
-
-        # set $PATH
-        export PATH=$MACOS_DOT_SCRIPTS:$PATH
-
-        export PATH="/usr/local/sbin:$PATH"
-
-        # export PATH=$CLANG:$PATH
-
-        # export PATH=$STACK_PACKAGES:$PATH
-
-        # export PATH=$REACT_NATIVE_HOME:$PATH
-
-        # export PATH=$MONO_ROOT:$PATH
-
-
-        # export PATH=$ANT_HOME/bin:$PATH
-        # export PATH=$MAVEN_HOME/bin:$PATH
-        # export PATH=$GRADLE_HOME/bin:$PATH
-
-        # export PATH=$ANDROID_HOME/tools:$PATH
-        # export PATH=$ANDROID_HOME/tools/bin:$PATH
-        # export PATH=$ANDROID_HOME/platform-tools:$PATH
-        # export PATH=$ANDROID_HOME/build-tools/23.0.1:$PATH
-
-      # -----------------------------------------------------------------------------
-      # Linux $PATH
-      # -----------------------------------------------------------------------------
-      else
-        # Linux dotfile scripts
-        export LINUX_DOT_SCRIPTS=$HOME/dotfiles/bin/linux
-
-        # set $PATH
-        export PATH=$LINUX_DOT_SCRIPTS:$PATH
-      fi
-
-
-      # }}}
-      # =============================================================================
-      # source and eval {{{
-      # =============================================================================
-
-      # aliases
-      source $HOME/dotfiles/conf/alias/.alias
-
-      if [[ -n "$DARWIN" ]]; then
-        source $HOME/dotfiles/conf/_darwin/alias/.alias
-      else
-        source $HOME/dotfiles/conf/_arch/alias/.alias
-      fi
-
-      # fasd
-      if which fasd > /dev/null; then eval "$(fasd --init auto)"; fi
-
-      # fnm
       eval "$(fnm env --multi)"
 
-      # npm tab completion
-      . <(npm completion)
-
-      # oh-my-zsh
-      source $ZSH/oh-my-zsh.sh
-
-      # rustup
-      # source $CARGO_HOME/env
-
-      # lorri direnv niv
       eval "$(direnv hook zsh)"
 
+      eval $(opam env)
 
-      # }}}
-      # =============================================================================
-      # fzf {{{
-      # =============================================================================
+      is_in_git_repo() {
+        git rev-parse HEAD > /dev/null 2>&1
+      }
 
-      # default options
-      # colors
-      # fullscreen
-      export FZF_DEFAULT_OPTS='
-        --color fg:7,bg:0,hl:3,fg+:4,bg+:0,hl+:3,info:2
-        --color prompt:6,spinner:4,pointer:3,marker:3,header:2
-        --no-height --no-reverse
-      '
+      fzf-git-wrapper() {
+        fzf --no-height --no-reverse "$@"
+      }
 
-      # make fzf use ripgrep
-      # --files: List files that would be searched but do not search
-      # --no-ignore: Do not respect .gitignore, etc...
-      # --hidden: Search hidden files and folders
-      # --follow: Follow symlinks
-      export FZF_DEFAULT_COMMAND='rg --files --hidden --follow 2> /dev/null'
+      gf() {
+        is_in_git_repo || return
+        git -c color.status=always status --short |
+        fzf-git-wrapper -m --ansi --nth 2..,.. \
+          --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+        cut -c4- | sed 's/.* -> //'
+      }
 
-      # use the same command with <C-t>
-      export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+      gb() {
+        is_in_git_repo || return
+        git branch -a --color=always | grep -v '/HEAD\s' | sort |
+        fzf-git-wrapper --ansi --multi --tac --preview-window right:70% \
+          --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+        sed 's/^..//' | cut -d' ' -f1 |
+        sed 's#^remotes/##'
+      }
 
-      # preview contents of file
-      # Using highlight (http://www.andre-simon.de/doku/highlight/en/highlight.html)
-      # see https://github.com/junegunn/fzf/wiki/Configuring-shell-key-bindings
-      export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
+      gt() {
+        is_in_git_repo || return
+        git tag --sort -version:refname |
+        fzf-git-wrapper --multi --preview-window right:70% \
+          --preview 'git show --color=always {} | head -'$LINES
+      }
 
-      # use the same command with <A-c>
-      export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+      gh() {
+        is_in_git_repo || return
+        git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+        fzf-git-wrapper --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+          --header 'Press CTRL-S to toggle sort' \
+          --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+        grep -o "[a-f0-9]\{7,\}"
+      }
 
-      [ -f $HOME/.nix-profile/share/fzf/key-bindings.zsh ] && source $HOME/.nix-profile/share/fzf/key-bindings.zsh
+      gr() {
+        is_in_git_repo || return
+        git remote -v | awk '{print $1 "\t" $2}' | uniq |
+        fzf-git-wrapper --tac \
+          --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+        cut -d$'\t' -f1
+      }
 
-      # }}}
+      join-lines() {
+        local item
+        while read item; do
+          echo -n "''${(q) item} "
+        done
+      }
 
-      # =============================================================================
-      # nix {{{
-      # =============================================================================
+      bind-git-helper() {
+        local char
+        for c in $@; do
+          eval "fzf-g$c-widget() { local result=\$(g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+          eval "zle -N fzf-g$c-widget"
+          eval "bindkey '^g^$c' fzf-g$c-widget"
+        done
+      }
 
-      # added by Nix installer
-      if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
-        . $HOME/.nix-profile/etc/profile.d/nix.sh;
-      fi
+      bind-git-helper f b t r h
+      unset -f bind-git-helper
 
-
-      # }}}
+      # profile zsh
+      # zprof
     '';
 
     shellAliases = {
-      # lazy
-      v = "nvim";
+      ":q" = "tmux kill-pane";
 
-      # vim with esy packages
+      perf = "for i in $(seq 1 10); do /usr/bin/time $SHELL -i -c exit; done";
+
+      hme = "home-manager edit";
+      hms = "home-manager switch";
+
+      v = "nvim";
+      vf = "nvim $(fzf)";
       ev = "esy nvim";
 
-      # open dotfiles in editor
-      dots = "cd $HOME/dotfiles && vim";
-
-      # open Neovim config
-      vimrc = "vim $HOME/dotfiles/conf/neovim";
-
-      # fzf -> open in vim
-      vf = "vim $(fzf)";
-
-      # open zsh config
-      zshrc = "vim $HOME/dotfiles/conf/zsh/.zshrc";
-
-      # open tmux config
-      tmuxrc = "vim $HOME/dotfiles/conf/tmux/.tmux.conf";
-
-      # open tmux split with swap files
+      dots = "cd $HOME/dotfiles && nvim";
       swap = "tmux split-window 'cd $HOME/.local/share/nvim/swap && nvim'";
 
-      # reload shell
       rl = "exec zsh";
 
-      # list npm modules global/local
-      ng = "npm list -g --depth=0 2>/dev/null";
-      nl = "npm list --depth=0 2>/dev/null";
-
-      # show ip and local ip
-      ip = "dig +short myip.opendns.com @resolver1.opendns.com";
-      ipl =
-        "ifconfig | grep -Eo 'inet (addr:)?([0-9]*.){3}[0-9]*' | grep -Eo '([0-9]*.){3}[0-9]*' | grep -v '127.0.0.1'";
-
-      # exclude macOS specific files in ZIP archives
-      zip = "zip -x *.DS_Store -x *__MACOSX* -x *.AppleDouble*";
-
-      # recursively remove Apple meta files
-      cleanupds = "find . -type f -name '*.DS_Store' -ls -exec /bin/rm {} ;";
-      cleanupad = "find . -type d -name '.AppleD*' -ls -exec /bin/rm -r {} ;";
-
-      # free WiFi
-      freewifi =
-        "sudo ifconfig en0 ether `openssl rand -hex 6 | sed 's/(..)/1:/g; s/.$//' `";
-
-      rwal =
-        "clear && wal -i $HOME/dropbox/walls -o $HOME/dotfiles/bin/_darwin/wal-set";
-      lrwal =
-        "clear && wal -l -i $HOME/dropbox/walls -o $HOME/dotfiles/bin/_darwin/wal-set";
-    };
-
-    oh-my-zsh = {
-      enable = true;
-      theme = "puck";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      "....." = "cd ../../../..";
     };
 
     plugins = [
-      {
-        name = "puck";
-        file = "puck.zsh-theme";
-        src = pkgs.fetchFromGitHub {
-          owner = "p1xelHer0";
-          repo = "puck";
-          rev = "da39a97292365c187c1eb778d8611a5ce46ad41c";
-          sha256 = "10m5y8y69nk4qnmplsjyk3s8j0iywjbwz5amw615bx7i99k0sqry";
-        };
-      }
-      {
-        name = "fast-syntax-highlighting";
-        file = "fast-syntax-highlighting.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "zdharma";
-          repo = "fast-syntax-highlighting";
-          rev = "v1.28";
-          sha256 = "106s7k9n7ssmgybh0kvdb8359f3rz60gfvxjxnxb4fg5gf1fs088";
-        };
-      }
       # {
-      #   name = "vi-mode";
-      #   file = "/plugins/vi-mode/vi-mode.plugin.zsh";
+      #   name = "puck";
+      #   file = "puck.zsh";
       #   src = pkgs.fetchFromGitHub {
-      #     owner = "ohmyzsh";
-      #     repo = "ohmyzsh";
-      #     rev = "3c777ebf1b24826363bea6bc29afa9ba038b10e8";
-      #     sha256 = "0pny3sadwn4v47d6alpzbffzics9xmpkwjnl8qb81lv1aipif8p3";
+      #     owner = "p1xelHer0";
+      #     repo = "puck";
+      #     rev = "";
+      #     sha256 = "";
       #   };
       # }
       {
-        name = "autopair";
-        file = "autopair.zsh";
+        name = "zsh-vim-mode";
+        file = "zsh-vim-mode.plugin.zsh";
         src = pkgs.fetchFromGitHub {
-          owner = "hlissner";
-          repo = "zsh-autopair";
-          rev = "4039bf142ac6d264decc1eb7937a11b292e65e24";
-          sha256 = "02pf87aiyglwwg7asm8mnbf9b2bcm82pyi1cj50yj74z4kwil6d1";
+          owner = "softmoth";
+          repo = "zsh-vim-mode";
+          rev = "1fb4fec7c38815e55bc1b33e7c2136069278c798";
+          sha256 = "1dxi18cpvbc96jl6w6j8r6zwpz8brjrnkl4kp8x1lzzariwm25sd";
         };
       }
+      # {
+      #   name = "fast-syntax-highlighting";
+      #   file = "fast-syntax-highlighting.plugin.zsh";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "zdharma";
+      #     repo = "fast-syntax-highlighting";
+      #     rev = "v1.28";
+      #     sha256 = "106s7k9n7ssmgybh0kvdb8359f3rz60gfvxjxnxb4fg5gf1fs088";
+      #   };
+      # }
     ];
   };
 
   programs.alacritty = {
     enable = true;
+
     settings = {
       window = {
         padding.x = 30;
@@ -480,9 +323,40 @@
     };
   };
 
+  programs.fzf = {
+    enable = true;
+
+    enableZshIntegration = true;
+
+    defaultCommand = "rg --files --hidden --follow 2> /dev/null";
+    defaultOptions = [''
+      --color fg:7,bg:0,hl:3,fg+:4,bg+:0,hl+:3,info:2
+      --color prompt:6,spinner:4,pointer:3,marker:3,header:2
+      --no-height --no-reverse
+    ''];
+
+    fileWidgetCommand = "rg --files --hidden --follow 2> /dev/null";
+    fileWidgetOptions = [
+      "--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
+    ];
+
+    changeDirWidgetCommand = "rg --files --hidden --follow 2> /dev/null";
+    changeDirWidgetOptions = [ ];
+
+    historyWidgetCommand = "";
+    historyWidgetOptions = [ ];
+  };
+
   programs.neovim = {
     enable = true;
+
     extraConfig = builtins.readFile ~/dotfiles/conf/nvim/.vimrc;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    withNodeJs = true;
 
     plugins = with pkgs.vimPlugins; [
       ale
@@ -493,6 +367,7 @@
       nerdtree-git-plugin
       supertab
       vim-commentary
+      vim-easy-align
       vim-fugitive
       vim-plug
       vim-repeat
@@ -537,5 +412,4 @@
     userEmail = "pontusnagy@gmail.com";
     includes = [{ path = "~/dotfiles/conf/git/.gitconfig"; }];
   };
-
 }
