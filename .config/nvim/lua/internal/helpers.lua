f3798ed40bcc502f57d6aca07777c54d88c5ec7b
Pontus Nagy <p_nagy@icloud.com>
2023-11-27 08:24:57 +0100

    nvim helpers

---
+++ .config/nvim/lua/internal/helpers.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+---Returns hex color of the value of the highlight group `hl_name`
+---@param hl_name string highlight group
+---@param value? string foreground/background
+---@return string | nil hex value
+M.get_highlight = function(hl_name, value)
+  if value == "fg" then
+    value = "foreground"
+  elseif value == "bg" then
+    value = "background"
+  end
+  value = value or "foreground"
+
+  vim.print(value)
+
+  local hl = vim.api.nvim_get_hl_by_name(hl_name, true)
+
+  if not hl[value] then
+    return nil
+  end
+
+  vim.print(hl[value])
+
+  local color = string.format("#%06x", hl[value])
+
+  vim.print(color)
+
+  return color
+end
+
+return M
