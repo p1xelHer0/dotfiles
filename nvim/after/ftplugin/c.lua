56ab786a09158c6e5782ffae2131824c581c88e4
Pontus Nagy <p1xelher0@p1xelBook.local>
2025-11-17 22:01:31 +0100

    c, objc, nvim

---
+++ nvim/after/ftplugin/c.lua
@@ -0,0 +1,6 @@
+local map = function(keys, func, desc, mode)
+  mode = mode or "n"
+  vim.keymap.set(mode, keys, func, { buffer = true, desc = "ft=c: " .. desc })
+end
+
+map("<Leader>c", "o////////////////////////////////////////<ESC>", "Comment section")
