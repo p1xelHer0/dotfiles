4b677c97efe5f509a614223182f157cd5de262f1
Pontus Nagy <p1xelher0@p1xelBook.local>
2025-12-12 21:18:18 +0100

    nvim: go

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,11 +1,13 @@
 -- we only ever have tabs in Go files: hide them
 -- stylua: ignore start
-vim.opt.listchars = {
-  eol             = " " ,
-  tab             = "  ",
-  trail           = "·" ,
-  extends         = "→" ,
-  precedes        = "←" ,
-  nbsp            = "␣" ,
+--vim.opt.shiftwidth = 2
+vim.opt.tabstop    = 2
+vim.opt.listchars  = {
+  eol              = " " ,
+  tab              = "  ",
+  trail            = "·" ,
+  extends          = "→" ,
+  precedes         = "←" ,
+  nbsp             = "␣" ,
 }
 -- stylua: ignore end
