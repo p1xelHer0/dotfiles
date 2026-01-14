1753a2172db55afee1e5e315c2dbd6b65c78aa84
p1xelHer0 <p_nagy@icloud.com>
2026-01-14 13:46:09 +0100

    nvim, odin: chore, also, its opt_local not local_opt

--- nvim/after/ftplugin/odin.lua
+++ nvim/after/ftplugin/odin.lua
@@ -4,15 +4,14 @@ local map = function(keys, func, desc, mode)
 end
 
 -- stylua: ignore start
-map("<Leader>s",      ":!./build_hot_reload.sh<CR>",                                                 "build hot reload DLL")
-map("<LocalLeader>s", ":!odin run % -file -- run metagen_rebuild<CR>",                               "build hot reload DLL")
-map("<Leader>S",      ":!odin build ./src/metagen -out:./bin/metagen -o:speed -no-bounds-check<CR>", "build metagen")
+map("<Leader>s",      ":!./b -- hot_reload<CR>",                                                     "build hot reload DLL")
+map("<LocalLeader>s", ":!odin run % -file -debug -- run metagen_rebuild hot_reload<CR>",             "build hot reload DLL")
 map("<Leader>c",      "o////////////////////////////////////////<ESC>",                              "comment section")
 -- stylua: ignore end
 
 -- stylua: ignore start
 vim.bo.tabstop          = 2
-vim.local_opt.listchars = { tab = "  " }
+vim.opt_local.listchars = { tab = "  " }
 -- stylua: ignore end
 
 vim.lsp.enable("ols")
